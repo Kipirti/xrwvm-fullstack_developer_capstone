e6ee92534837be6921dd03dce1deb7a7e3cfdfaf
Kipirti <ersintavli@gmail.com>
2024-09-16 02:45:45 -0400

    Adding temporary changes to Github

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -6,10 +6,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 backend_url = os.getenv(
-    'backend_url', default="https://ersintavli-3030.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai")
+    'backend_url', default="https://ersintavli-3030.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai")
 sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
-    default="https://sentianalyzer.1ltreewfovpm.us-south.codeengine.appdomain.cloud/")
+    default="https://sentianalyzer.1lwgmf2dpgne.us-south.codeengine.appdomain.cloud/")
 
 # def get_request(endpoint, **kwargs):
 # Add code for get requests to back end
